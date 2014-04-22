4521be9fcc9fd8a42091242f74e6b6f95c337deb
WJsjtu <jenniswonstju@gmail.com>
2014-04-22 23:38:13 +0800

    联系人高度抽象化，使用方式见ContactListActivity

---
+++ single demo/拨打电话/Call/app/Contact.java
@@ -0,0 +1,14 @@
+package com.call.app;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by wjbnys on 14-4-22.
+ */
+public class Contact {
+    public long id = 0;
+    public long photoid = 0;
+    public String name = "";
+    public String number = "";
+    public Bitmap photo = null;
+}
