6d77247ccfb9cc3ce84706582d093c2f84b8eb28
feng-zhe <henryzhefeng@gmail.com>
2014-04-22 08:06:58 +0800

    能从preference中读取了

--- src/src/com/example/forgrandmon/PhoneActivity.java
+++ src/src/com/example/forgrandmon/PhoneActivity.java
@@ -1,8 +1,11 @@
 package com.example.forgrandmon;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -12,10 +15,21 @@ public class PhoneActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_phone);
-		// add buttons to it
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// get text
+		// name should be <package>_preferences
+		String PREF_NAME = "com.example.forgrandmon_preferences";
+		SharedPreferences settings = getSharedPreferences(PREF_NAME, 0);
+		// we can't retrieve the android:defaultValue...Don't know why
+		String phone = settings.getString("webAccount", "Wrong!");
+		// add buttonwebAccount
 		LinearLayout layout = (LinearLayout) findViewById(R.id.phone_list);
 		Button btTest = new Button(this);
-		btTest.setText("added button");
+		btTest.setText(phone);
 		layout.addView(btTest);
 	}
 
