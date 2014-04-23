83cc653f3d3e748716aefac9b76b9e8c4daeaa52
feng-zhe <henryzhefeng@gmail.com>
2014-04-23 10:50:11 +0800

    Photo parts is done

--- src/src/com/example/forgrandmon/PhotoActivity.java
+++ src/src/com/example/forgrandmon/PhotoActivity.java
@@ -1,13 +1,28 @@
 package com.example.forgrandmon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class PhotoActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photo);
+	}
+
+	public void onBtNewClicked(View v) {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, 1);
+	}
+
+	public void onBtOldClicked(View v) {
+		Intent intent = new Intent(Intent.ACTION_PICK, null);
+		intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+				 "image/*");
+		startActivityForResult(intent, 2);
 	}
 }
